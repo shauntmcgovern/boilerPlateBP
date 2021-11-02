cdb198a8550332771744eec34393671538765391
Shaun McGovern <shauntmcgovern@gmail.com>
2021-11-02 13:51:54 -0500

    Update and rename index.jsx to Index.jsx

--- app/router/Index.jsx
+++ app/router/Index.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
-import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+import {Route, Router, indexRoute, hashHistory} from 'react-router';
 
-import TodoApp from 'TodoApp';
+import toDoApp from 'toDoApp';
 import Login from 'Login';
-import firebase from 'app/firebase/';
+import fireBase from 'app/fireBase/';
 
 var requireLogin = (nextState, replace, next) => {
-  if (!firebase.auth().currentUser) {
+  if (!fireBase.auth().currentUser) {
     replace('/');
   }
   next();
 };
 
 var redirectIfLoggedIn = (nextState, replace, next) => {
-  if (firebase.auth().currentUser) {
-    replace('/todos');
+  if (fireBase.auth().currentUser) {
+    replace('/toDos');
   }
-
   next();
 };
 
 export default (
   <Router history={hashHistory}>
     <Route path="/">
-      <Route path="todos" component={TodoApp} onEnter={requireLogin}/>
+      <Route path="toDos" component={toDoApp} onEnter={requireLogin}/>
       <IndexRoute component={Login} onEnter={redirectIfLoggedIn}/>
     </Route>
   </Router>
