bb7abd97b404ca90a4c6c2618aaa35e807971dbd
Shaun McGovern <shauntmcgovern@gmail.com>
2022-01-04 15:17:18 -0600

    Update spread.js

--- playground/spread.js
+++ playground/spread.js
@@ -7,15 +7,15 @@
 // var toAdd = [9, 5];
 // console.log(add(...toAdd));
 
-// var groupA = ['Jen', 'Cory'];
-// var groupB = ['Vikram'];
+// var groupA = ['Logs', 'Branches'];
+// var groupB = ['Sticks'];
 // var final = [...groupB, 3, ...groupA];
 //
 // console.log(final);
 
-var person = ['Andrew', 25];
-var personTwo = ['Jen', 29];
-// Hi Andrew, you are 25
+var person = ['Charcoal', 8];
+var personTwo = ['Logs', 35];
+// Hi ShaunPersonNamedShaun, you are 35
 
 function greet (name, age) {
   console.log('Hi ' + name + ', you are ' + age);
@@ -23,9 +23,9 @@ function greet (name, age) {
 greet(...person);
 greet(...personTwo);
 
-var names = ['Mike', 'Ben'];
-var final = ['Andrew', ...names];
-// Hi Andrew
+var names = ['Charcoal', 'Logs'];
+var final = ['Fire', ...names];
+// Hi Shaun
 
 final.forEach(function (name) {
   console.log('Hi ' + name);
