edac3a2381702f2c19c7dc40d99271b71eae8fbd
Shaun McGovern <shauntmcgovern@gmail.com>
2022-01-04 15:18:25 -0600

    Update moment-examples.js

--- playground/moment-examples.js
+++ playground/moment-examples.js
@@ -2,9 +2,8 @@ var moment = require('moment');
 
 console.log(moment().format());
 
-// January 1st 1970 @ 12:00am -> 0
-// January 1st 1970 @ 12:01am -> -60
-
+// January 4th 2022 @3:30pm -> -60
+// January 5h 2022 @3:30pm -> -60
 var now = moment();
 console.log('Current timestamp', now.unix());
 
